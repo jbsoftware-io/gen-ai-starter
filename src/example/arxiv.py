4883f98f9f19e3544138f818110e80cd4830ca5b
Jason Bilyeu <jbilyeu@mac.lan>
2025-04-04 21:45:33 -0500

    feat: adds arxiv example

---
+++ src/example/arxiv.py
@@ -0,0 +1,60 @@
+import logging
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.retrievers import ArxivRetriever
+from internal.prompts import create_summarize_prompt_v2
+from internal.util import create_llm, format_docs
+
+
+def handle_arxiv(st, model_name):
+    search_query = st.text_input(
+        "Question",
+        placeholder="Ask a question about scientific or engineering research."
+    )
+
+    if st.button("Summarize"):
+        with st.spinner('Please wait...'):
+            try:
+                if not search_query:
+                    st.warning("Please enter a question.")
+                    return
+
+                llm = create_llm(model_name)
+
+                # summarize_prompt = create_summarize_prompt()
+                summarize_prompt = create_summarize_prompt_v2()
+
+                rag_chain_from_docs = (
+                    RunnablePassthrough.assign(
+                        context=(lambda x: format_docs(x["context"])))
+                    | summarize_prompt
+                    | llm
+                    | StrOutputParser()
+                )
+
+                retriever = ArxivRetriever(
+                    load_max_docs=3,
+                    get_full_documents=True,
+                )
+
+                retrieve_docs = (
+                    lambda x: x["question"]
+                ) | retriever
+
+                logging.info("Invoking chain")
+                chain = RunnablePassthrough.assign(
+                    context=retrieve_docs
+                ).assign(answer=rag_chain_from_docs)
+
+                result = chain.invoke({"question": search_query})
+
+                logging.info("Result")
+                logging.info(result)
+                logging.info('-'*30)
+
+                if not result or not result['answer']:
+                    st.warning("No answer was found.")
+                else:
+                    st.success(result['answer'])
+            except Exception as e:
+                st.exception(f"An error occurred: {e}")
