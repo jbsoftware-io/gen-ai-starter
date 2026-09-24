7173a5ed4417c027a18f7b7aac0a97cb71d78726
Jason Bilyeu <jasonbilyeu@users.noreply.github.com>
2026-09-24 10:11:27 -0500

    fix: always use nomic-text-embed when embedding for vectorization (#102)
    Co-authored-by: Jason Bilyeu <jbilyeu@mac.lan>

--- etc/ollama_entrypoint.sh
+++ etc/ollama_entrypoint.sh
@@ -9,7 +9,7 @@ pid=$!
 sleep 5
 
 # Read models from OLLAMA_MODELS env variable, default if not set
-MODELS=${OLLAMA_MODELS:-"llama3.2;mistral;gemma4:e4b"}
+MODELS=${OLLAMA_MODELS:-"llama3.2;mistral;gemma4:e4b;nomic-embed-text"}
 
 # Loop through models and pull each
 IFS=';' read -ra MODEL_LIST <<< "$MODELS"
